b58e3cf6da5d2d3e15d1b4092dc5296e262e29a1
Gael Arrambide <gael_arrambide@hotmail.com>
2016-10-22 10:49:33 -0500

    Server working

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//route to home page
+/*app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'home.html'));
+});*/
+app.get('/', function (req, res) {
+	res.send('Welcome to Hot Restaurant!');
+});
+
+//Starts the server to begin listening
+app.listen(PORT, function () {
+	console.log('App listening on PORT ' + PORT);
+});
