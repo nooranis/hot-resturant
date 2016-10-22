3f9109d30e095e59f8bbb56d2afe115d44a04d40
Gael Arrambide <gael_arrambide@hotmail.com>
2016-10-22 11:17:03 -0500

    routes

--- app.js
+++ app.js
@@ -13,12 +13,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+var reservations=[{
+    customerName: "Gael Arrambide",
+    phoneNumber:"9566154895",
+    customerEmail: "gael_arrambide.com",
+    customerID: "777"
+  },
+  {
+    customerName: "Carlos Lopez",
+    phoneNumber:"55525552",
+    customerEmail: "clo2go@gmail.com",
+    customerID: "512"
+    },
+];
+
 //route to home page
-/*app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'home.html'));
-});*/
 app.get('/', function (req, res) {
-	res.send('Welcome to Hot Restaurant!');
+	res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+//route to tables page
+app.get('/tables.html', function (req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+//route to reserve
+app.get('/reserve.html', function (req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'));
 });
 
 //Starts the server to begin listening
