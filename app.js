f61436c2e1f68da04cbd349844dd0851022b1fb7
Gael Arrambide <gael_arrambide@hotmail.com>
2016-10-22 13:27:16 -0500

    adding post and get

--- app.js
+++ app.js
@@ -42,6 +42,38 @@ app.get('/reserve', function (req, res) {
 	res.sendFile(path.join(__dirname, 'reserve.html'));
 });
 
+// Search for Specific Character (or all characters) - provides JSON
+app.get('/api/:reservations?', function (req, res) {
+	var chosen = req.params.characters;
+
+	if (chosen) {
+		console.log(chosen);
+
+		for (var i = 0; i < reservations.length; i++) {
+			if (chosen === reservations[i].routeName) {
+				res.json(reservations[i]);
+				return;
+			}
+		}
+
+		res.json(false);
+	} else {
+		res.json(reservations);
+	}
+});
+
+// Create New Characters - takes in JSON input
+app.post('/api/new', function (req, res) {
+	var newReservation = req.body;
+	newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
+
+	console.log(newReservation);
+
+	characters.push(newReservation);
+
+	res.json(newReservation);
+});
+
 //Starts the server to begin listening
 app.listen(PORT, function () {
 	console.log('App listening on PORT ' + PORT);
